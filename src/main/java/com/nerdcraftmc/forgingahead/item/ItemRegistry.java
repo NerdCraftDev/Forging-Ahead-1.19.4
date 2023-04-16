e85b42cd36c7d0d1642a0dc3de888ade66b97f9b
Ben Rose <daeruin@benjaminrose.com>
2023-04-16 11:01:49 -0600

    Add powder items

--- src/main/java/com/nerdcraftmc/forgingahead/item/ItemRegistry.java
+++ src/main/java/com/nerdcraftmc/forgingahead/item/ItemRegistry.java
@@ -12,6 +12,24 @@ public class ItemRegistry {
 
     public static final RegistryObject<Item> FYRALITE_POWDER = ITEMS.register("fyralite_powder",
             () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> COAL_POWDER = ITEMS.register("coal_powder",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> COPPER_POWDER = ITEMS.register("copper_powder",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> IRON_POWDER = ITEMS.register("iron_powder",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> GOLD_POWDER = ITEMS.register("gold_powder",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> LAPIS_LAZULI_POWDER = ITEMS.register("lapis_powder",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> NETHER_QUARTZ_POWDER = ITEMS.register("quartz_powder",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> EMERALD_POWDER = ITEMS.register("emerald_powder",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> DIAMOND_POWDER = ITEMS.register("diamond_powder",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> NETHERITE_POWDER = ITEMS.register("netherite_powder",
+            () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> COAL_ALLOY = ITEMS.register("coal_alloy",
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> COPPER_ALLOY = ITEMS.register("copper_alloy",
